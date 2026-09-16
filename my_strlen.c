6ce5af6cec95dee2c3c74fa1fcab62e98c630090
12temm <bediakomelvin@gmail.com>
2026-09-16 12:35:47 +0200

    fix

--- my_strlen.c
+++ my_strlen.c
@@ -1,4 +1,13 @@
-int size_t;
-int my_strlen(const char size_t){
-    return size_t;
+int
+my_strlen(const char *size_t)
+{
+	int length = 0;
+	if (size_t[length] == '\0') {
+		return 0;
+	} else {
+		while (size_t[length] != '\0') {
+			length++;
+		}
+	}
+	return length;
 };
