ab33caaa47319d63c47f0de1c34a7f38f339542e
12temm <bediakomelvin@gmail.com>
2026-09-17 10:02:57 +0200

    fix clang-format -i my_libc.h~

--- my_libc.h
+++ my_libc.h
@@ -1,14 +1,15 @@
 #include <stddef.h>
 
-size_t my_strlen(const char *str)
+size_t
+my_strlen(const char *str)
 {
-    size_t length = 0;
-    if (str[length] == '\0') {
-        return 0;
-    } else {
-        while (str[length] != '\0') {
-            length++;
-        }
-    }
-    return length;
+	size_t length = 0;
+	if (str[length] == '\0') {
+		return 0;
+	} else {
+		while (str[length] != '\0') {
+			length++;
+		}
+	}
+	return length;
 };
