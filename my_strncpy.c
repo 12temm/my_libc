c7e137039947d796fa41f9aaa9aad99d79d6aab3
12temm <bediakomelvin@gmail.com>
2026-09-17 09:13:02 +0200

    strncopy

---
+++ my_strncpy.c
@@ -0,0 +1,13 @@
+char *
+my_strncpy(char *dst, const char *src, int n)
+{
+	int i;
+	for (i = 0; i < n && src[i] != '\0'; i++) {
+		dst[i] = src[i];
+	}
+	while (i < n) {
+		dst[i] = '\0';
+		i++;
+	}
+	return dst;
+}
