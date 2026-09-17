74dfdd103b5ede84d398ba2b9f0ed26e35956897
12temm <bediakomelvin@gmail.com>
2026-09-17 10:06:47 +0200

    fix my_lib.h

--- my_libc.h
+++ my_libc.h
@@ -1,15 +1,12 @@
+#ifndef MY_LIBC_H
+#define MY_LIBC_H
+
 #include <stddef.h>
 
-size_t
-my_strlen(const char *str)
-{
-	size_t length = 0;
-	if (str[length] == '\0') {
-		return 0;
-	} else {
-		while (str[length] != '\0') {
-			length++;
-		}
-	}
-	return length;
-};
+size_t my_strlen(const char *str);
+
+char *my_strcpy(char *dest, const char *src);
+
+char *my_strncpy(char *dst, const char *src, int n);
+
+#endif
